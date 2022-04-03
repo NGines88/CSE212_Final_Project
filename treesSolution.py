9e08ad13fd35f72bf7f810ae50d8b852ff67f95a
NGines88 <nickgines88@yahoo.com>
2022-04-02 21:47:31 -0600

    Fix trees comments

--- treesSolution.py
+++ treesSolution.py
@@ -7,9 +7,8 @@ my_object = {'E':5, 'F':6, 'B':2, 'D':4, 'A':1, 'C':3}
 # Create the tree using the object
 my_tree = AVLTree(my_object)
 
-# Prints each successive item after the lowest value in the tree
+# Prints each highest item until no higher item than the lowest exists
 def print_tree(k, v):
-    # Do not print beyond the last value
     print(my_tree.pop_max())
 my_tree.foreach(print_tree)
 
